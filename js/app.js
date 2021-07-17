b841b222df4372fa7ca570efcd3375eace3167c6
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-07-18 01:40:02 +0200

    using webgl renderer instead of canvas renderer

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 var NB_STRIPES = 13;
 var COLORS = [
-    "#f0c101",
-    "#90b623",
-    "#6aa74a",
-    "#4e9577",
-    "#357ab5",
-    "#4067aa",
-    "#7b426b",
-    "#974971",
-    "#b4534d",
-    "#c35530",
-    "#d1672b",
-    "#e19202",
-    "#eebf00"
+    { r: 240 / 255, g: 193 / 255, b: 1 / 255 },
+    { r: 144 / 255, g: 182 / 255, b: 35 / 255 },
+    { r: 106 / 255, g: 167 / 255, b: 74 / 255 },
+    { r: 78 / 255, g: 149 / 255, b: 119 / 255 },
+    { r: 53 / 255, g: 122 / 255, b: 181 / 255 },
+    { r: 64 / 255, g: 103 / 255, b: 170 / 255 },
+    { r: 123 / 255, g: 66 / 255, b: 107 / 255 },
+    { r: 151 / 255, g: 73 / 255, b: 113 / 255 },
+    { r: 180 / 255, g: 83 / 255, b: 77 / 255 },
+    { r: 195 / 255, g: 85 / 255, b: 48 / 255 },
+    { r: 209 / 255, g: 103 / 255, b: 43 / 255 },
+    { r: 225 / 255, g: 146 / 255, b: 2 / 255 },
+    { r: 238 / 255, g: 191 / 255, b: 0 / 255 },
 ];
+var VERTEX_SHADER_ID = "vertex-shader-2d";
+var FRAGMENT_SHADER_ID = "fragment-shader-2d";
 function getCanvas() {
     var canvas = document.getElementById("canvas");
     if (!canvas) {
@@ -22,49 +24,145 @@ function getCanvas() {
     return canvas;
 }
 function getContext(canvas) {
-    var context = canvas.getContext("2d");
+    var context = canvas.getContext("webgl");
     if (!context) {
         throw "cannot find the context in the page";
     }
     return context;
 }
-function drawFrame(context, thicknessFrame) {
-    var width = context.canvas.width;
-    var thicknessOuterFrame = thicknessFrame / 4;
-    context.save();
-    context.fillRect(0, 0, width, width);
-    context.clearRect(thicknessOuterFrame, thicknessOuterFrame, width - (thicknessOuterFrame * 2), width - (thicknessOuterFrame * 2));
-    context.fillRect(thicknessOuterFrame * 2, thicknessOuterFrame * 2, width - (thicknessOuterFrame * 4), width - (thicknessOuterFrame * 4));
-    context.restore();
-}
-function drawColorStripe(context, xOrigin, yOrigin, width, height, color) {
-    context.save();
-    context.fillStyle = color;
-    context.fillRect(xOrigin, yOrigin, width, height);
-    context.restore();
-}
-function draw() {
-    var canvas = getCanvas();
-    var context = getContext(canvas);
-    context.canvas.width = window.innerHeight;
-    context.canvas.height = window.innerHeight;
-    var frameThickness = 150;
-    drawFrame(context, frameThickness);
-    var _a = context.canvas, width = _a.width, height = _a.height;
-    var stripeWidth = (width - frameThickness) / NB_STRIPES;
-    var rest = (width - (2.0 * frameThickness)) % NB_STRIPES;
-    console.log(stripeWidth);
-    console.log(height);
-    console.log(window.innerHeight);
-    for (var index = 0; index < NB_STRIPES; ++index) {
-        drawColorStripe(context, frameThickness / 2 + stripeWidth * index, frameThickness / 2, stripeWidth, height - frameThickness, COLORS[index]);
+function createShader(webGLContext, type, source) {
+    var shader = webGLContext.createShader(type);
+    if (!shader) {
+        throw "the shader cannot be created";
     }
+    webGLContext.shaderSource(shader, source);
+    webGLContext.compileShader(shader);
+    var success = webGLContext.getShaderParameter(shader, webGLContext.COMPILE_STATUS);
+    if (!success) {
+        console.error(webGLContext.getShaderInfoLog(shader));
+        webGLContext.deleteShader(shader);
+        throw "the shader has not the been compiled";
+    }
+    return shader;
 }
-function reportWindowSize() {
-    draw();
+function createProgram(webGLContext, vertexShader, fragmentShader) {
+    var program = webGLContext.createProgram();
+    if (!program) {
+        throw "the program cannot be created";
+    }
+    webGLContext.attachShader(program, vertexShader);
+    webGLContext.attachShader(program, fragmentShader);
+    webGLContext.linkProgram(program);
+    var success = webGLContext.getProgramParameter(program, webGLContext.LINK_STATUS);
+    if (!success) {
+        console.error(webGLContext.getProgramInfoLog(program));
+        webGLContext.deleteProgram(program);
+        throw "the program cannot be built";
+    }
+    return program;
 }
-window.onresize = reportWindowSize;
+function resizeCanvasToDisplaySize(canvas) {
+    // Lookup the size the browser is displaying the canvas in CSS pixels.
+    var displayWidth = canvas.clientWidth;
+    var displayHeight = canvas.clientHeight;
+    // Check if the canvas is not the same size.
+    var needResize = canvas.width !== displayWidth ||
+        canvas.height !== displayHeight;
+    if (needResize) {
+        // Make the canvas the same size
+        canvas.width = displayWidth;
+        canvas.height = displayHeight;
+    }
+    return needResize;
+}
+function setRectangle(webGLContext, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+    webGLContext.bufferData(webGLContext.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2,
+    ]), webGLContext.STATIC_DRAW);
+}
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+    return Math.floor(Math.random() * range);
+}
 window.addEventListener("load", function (event) {
-    draw();
+    /// --------LOAD THE SHADERS---------------- ///
+    var vertexShaderSourceNode = document.getElementById(VERTEX_SHADER_ID);
+    if (!vertexShaderSourceNode) {
+        console.error("Cannot find id " + VERTEX_SHADER_ID + " in the dom");
+        return;
+    }
+    var vertexShaderSource = vertexShaderSourceNode.innerText;
+    var fragmentShaderSourceNode = document.getElementById(FRAGMENT_SHADER_ID);
+    if (!fragmentShaderSourceNode) {
+        console.error("Cannot find id " + FRAGMENT_SHADER_ID + " in the dom");
+        return;
+    }
+    var fragmentShaderSource = fragmentShaderSourceNode.innerText;
+    /// ------------------------ ///
+    var canvas = getCanvas();
+    canvas.width = window.innerHeight;
+    canvas.height = window.innerHeight;
+    var webGLContext = getContext(canvas);
+    var vertexShader = createShader(webGLContext, webGLContext.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(webGLContext, webGLContext.FRAGMENT_SHADER, fragmentShaderSource);
+    var program = createProgram(webGLContext, vertexShader, fragmentShader);
+    // look up where the vertex data needs to go.
+    var positionAttributeLocation = webGLContext.getAttribLocation(program, "a_position");
+    var resolutionUniformLocation = webGLContext.getUniformLocation(program, "u_resolution");
+    // color
+    var colorUniformLocation = webGLContext.getUniformLocation(program, "u_color");
+    var timeUniformLocation = webGLContext.getUniformLocation(program, "u_time");
+    // Create a buffer and put three 2d clip space points in it
+    var positionBuffer = webGLContext.createBuffer();
+    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+    webGLContext.bindBuffer(webGLContext.ARRAY_BUFFER, positionBuffer);
+    // code above this line is initialization code.
+    // code below this line is rendering code.
+    resizeCanvasToDisplaySize(canvas);
+    // Tell WebGL how to convert from clip space to pixels
+    webGLContext.viewport(0, 0, canvas.width, canvas.height);
+    // Clear the canvas
+    webGLContext.clearColor(0, 0, 0, 0);
+    webGLContext.clear(webGLContext.COLOR_BUFFER_BIT);
+    // Tell it to use our program (pair of shaders)
+    webGLContext.useProgram(program);
+    // Turn on the attribute
+    webGLContext.enableVertexAttribArray(positionAttributeLocation);
+    // Bind the position buffer.
+    webGLContext.bindBuffer(webGLContext.ARRAY_BUFFER, positionBuffer);
+    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2; // 2 components per iteration
+    var type = webGLContext.FLOAT; // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0; // start at the beginning of the buffer
+    webGLContext.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+    // set the resolution
+    webGLContext.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
+    var stripeWidth = (canvas.width) / NB_STRIPES;
+    for (var index = 0; index < NB_STRIPES; ++index) {
+        // Setup a random rectangle
+        // This will write to positionBuffer because
+        // its the last thing we bound on the ARRAY_BUFFER
+        // bind point
+        setRectangle(webGLContext, index * stripeWidth, 0, stripeWidth, canvas.height);
+        // Set a random color.
+        webGLContext.uniform4f(colorUniformLocation, COLORS[index].r, COLORS[index].g, COLORS[index].b, 1);
+        webGLContext.uniform1f(timeUniformLocation, Math.random());
+        // Draw the rectangle.
+        var primitiveType = webGLContext.TRIANGLES;
+        var offset_1 = 0;
+        var count = 6;
+        webGLContext.drawArrays(primitiveType, offset_1, count);
+    }
 });
 //# sourceMappingURL=app.js.map
--- js/app.ts
+++ js/app.ts
@@ -1,19 +1,22 @@
 const NB_STRIPES = 13;
 const COLORS = [
-  "#f0c101",
-  "#90b623",
-  "#6aa74a",
-  "#4e9577",
-  "#357ab5",
-  "#4067aa",
-  "#7b426b",
-  "#974971",
-  "#b4534d",
-  "#c35530",
-  "#d1672b",
-  "#e19202",
-  "#eebf00"
+  {r: 240/255, g:193/255, b: 1/255},
+  {r: 144/255, g:182/255, b: 35/255},
+  {r: 106/255, g:167/255, b: 74/255},
+  {r: 78/255, g:149/255, b: 119/255},
+  {r: 53/255, g:122/255, b: 181/255},
+  {r: 64/255, g:103/255, b: 170/255},
+  {r: 123/255, g:66/255, b: 107/255},
+  {r: 151/255, g:73/255, b: 113/255},
+  {r: 180/255, g:83/255, b: 77/255},
+  {r: 195/255, g:85/255, b: 48/255},
+  {r: 209/255, g:103/255, b: 43/255},
+  {r: 225/255, g:146/255, b: 2/255},
+  {r: 238/255, g:191/255, b: 0/255},
 ]
+
+const VERTEX_SHADER_ID = "vertex-shader-2d";
+const FRAGMENT_SHADER_ID = "fragment-shader-2d";
 
 
 function getCanvas() : HTMLCanvasElement {
@@ -24,66 +27,191 @@ function getCanvas() : HTMLCanvasElement {
   return canvas;
 }
 
-function getContext(canvas: HTMLCanvasElement) : CanvasRenderingContext2D {
-  let context = canvas.getContext("2d");
+function getContext(canvas: HTMLCanvasElement) : WebGLRenderingContext {
+  let context = canvas.getContext("webgl");
   if(!context) {
     throw "cannot find the context in the page";
   }
   return context;
 }
 
-function drawFrame(context: CanvasRenderingContext2D, thicknessFrame: number)  {
-  const { canvas: { width } } =  context;
-  const thicknessOuterFrame = thicknessFrame / 4;
-  context.save();
 
-  context.fillRect(0, 0, width, width);
-  context.clearRect(thicknessOuterFrame, thicknessOuterFrame, width - (thicknessOuterFrame * 2), width - (thicknessOuterFrame * 2));
-  context.fillRect(thicknessOuterFrame * 2, thicknessOuterFrame * 2, width - (thicknessOuterFrame * 4), width - (thicknessOuterFrame * 4));
-
-  context.restore();
-
-}
-
-function drawColorStripe(context: CanvasRenderingContext2D, xOrigin : number, yOrigin: number, width: number, height: number, color: string) {
-  context.save();
-  context.fillStyle = color;
-  context.fillRect(xOrigin, yOrigin, width, height);
-  context.restore();
-}
-
-
-function draw() {
-  let canvas = getCanvas();
-  let context = getContext(canvas);
-
-  context.canvas.width  = window.innerHeight;
-  context.canvas.height = window.innerHeight;
-
-  const frameThickness = 150;
-  drawFrame(context, frameThickness);
-
-  const { canvas: { width, height } } =  context;
-  const stripeWidth = (width -  frameThickness) / NB_STRIPES;
-  const rest = (width - (2.0 * frameThickness)) % NB_STRIPES;
-
-  console.log(stripeWidth)
-  console.log(height)
-  console.log(window.innerHeight)
-  for(let index = 0; index < NB_STRIPES; ++index) {
-    drawColorStripe(context, frameThickness/2 + stripeWidth * index, frameThickness/2, stripeWidth, height - frameThickness, COLORS[index]);
+function createShader(webGLContext: WebGLRenderingContext, type: number, source: string): WebGLShader {
+  let shader = webGLContext.createShader(type);
+  if(!shader) {
+    throw "the shader cannot be created";
+  }
+  webGLContext.shaderSource(shader, source);
+  webGLContext.compileShader(shader);
+  let success = webGLContext.getShaderParameter(shader, webGLContext.COMPILE_STATUS);
+  
+  if(!success) {
+    console.error(webGLContext.getShaderInfoLog(shader));
+    webGLContext.deleteShader(shader);
+    throw "the shader has not the been compiled";
   }
 
+  return shader;
+}
+
+function createProgram(webGLContext: WebGLRenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) : WebGLProgram {
+  let program = webGLContext.createProgram();
+  if(!program) {
+    throw "the program cannot be created";
+  }
+
+  webGLContext.attachShader(program, vertexShader);
+  webGLContext.attachShader(program, fragmentShader);
+  webGLContext.linkProgram(program);
+  
+  const success = webGLContext.getProgramParameter(program, webGLContext.LINK_STATUS);
+  if (!success) {
+    console.error(webGLContext.getProgramInfoLog(program));
+    webGLContext.deleteProgram(program);
+    throw "the program cannot be built"
+  }
+ 
+  return program;
 }
 
 
-function reportWindowSize() {
-  draw();
+function resizeCanvasToDisplaySize(canvas : HTMLCanvasElement) : boolean {
+  // Lookup the size the browser is displaying the canvas in CSS pixels.
+  const displayWidth  = canvas.clientWidth;
+  const displayHeight = canvas.clientHeight;
+ 
+  // Check if the canvas is not the same size.
+  const needResize = canvas.width  !== displayWidth ||
+                     canvas.height !== displayHeight;
+ 
+  if (needResize) {
+    // Make the canvas the same size
+    canvas.width  = displayWidth;
+    canvas.height = displayHeight;
+  }
+ 
+  return needResize;
+}
+
+function setRectangle(webGLContext: WebGLRenderingContext, x: number, y: number, width : number, height: number) : void {
+  const x1 = x;
+  const x2 = x + width;
+  const y1 = y;
+  const y2 = y + height;
+  webGLContext.bufferData(webGLContext.ARRAY_BUFFER, new Float32Array([
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2,
+  ]), webGLContext.STATIC_DRAW);
 }
 
 
-window.onresize = reportWindowSize;
+// Returns a random integer from 0 to range - 1.
+function randomInt(range: number) {
+  return Math.floor(Math.random() * range);
+}
+
 
 window.addEventListener("load", function(event) {
-  draw();
+  /// --------LOAD THE SHADERS---------------- ///
+  const vertexShaderSourceNode = document.getElementById(VERTEX_SHADER_ID);
+  if(!vertexShaderSourceNode) {
+    console.error(`Cannot find id ${VERTEX_SHADER_ID} in the dom`)
+    return;
+  }
+  const vertexShaderSource = vertexShaderSourceNode.innerText;
+  
+  const fragmentShaderSourceNode = document.getElementById(FRAGMENT_SHADER_ID)
+  if(!fragmentShaderSourceNode) {
+    console.error(`Cannot find id ${FRAGMENT_SHADER_ID} in the dom`)
+    return;
+  }
+  const fragmentShaderSource = fragmentShaderSourceNode.innerText;
+  /// ------------------------ ///
+
+  let canvas = getCanvas();
+  canvas.width  = window.innerHeight;
+  canvas.height = window.innerHeight;
+
+  let webGLContext = getContext(canvas);
+
+  const vertexShader = createShader(webGLContext, webGLContext.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = createShader(webGLContext, webGLContext.FRAGMENT_SHADER, fragmentShaderSource);
+
+  const program = createProgram(webGLContext, vertexShader, fragmentShader);
+
+  // look up where the vertex data needs to go.
+  const positionAttributeLocation = webGLContext.getAttribLocation(program, "a_position");
+  const resolutionUniformLocation = webGLContext.getUniformLocation(program, "u_resolution");
+
+  // color
+  const colorUniformLocation = webGLContext.getUniformLocation(program, "u_color");
+  const timeUniformLocation = webGLContext.getUniformLocation(program, "u_time");
+  // Create a buffer and put three 2d clip space points in it
+  const positionBuffer = webGLContext.createBuffer();
+
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  webGLContext.bindBuffer(webGLContext.ARRAY_BUFFER, positionBuffer);
+
+  // code above this line is initialization code.
+  // code below this line is rendering code.
+
+  resizeCanvasToDisplaySize(canvas);
+
+  // Tell WebGL how to convert from clip space to pixels
+  webGLContext.viewport(0, 0, canvas.width, canvas.height);
+
+  // Clear the canvas
+  webGLContext.clearColor(0, 0, 0, 0);
+  webGLContext.clear(webGLContext.COLOR_BUFFER_BIT);
+
+  // Tell it to use our program (pair of shaders)
+  webGLContext.useProgram(program);
+
+  // Turn on the attribute
+  webGLContext.enableVertexAttribArray(positionAttributeLocation);
+
+  // Bind the position buffer.
+  webGLContext.bindBuffer(webGLContext.ARRAY_BUFFER, positionBuffer);
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  const size = 2;          // 2 components per iteration
+  const type = webGLContext.FLOAT;   // the data is 32bit floats
+  const normalize = false; // don't normalize the data
+  const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  const offset = 0;        // start at the beginning of the buffer
+  webGLContext.vertexAttribPointer(
+    positionAttributeLocation,
+    size,
+    type,
+    normalize,
+    stride,
+    offset
+  );
+
+  // set the resolution
+  webGLContext.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
+
+  const stripeWidth = (canvas.width) / NB_STRIPES;
+  for (let index = 0; index < NB_STRIPES; ++index) {
+    // Setup a random rectangle
+    // This will write to positionBuffer because
+    // its the last thing we bound on the ARRAY_BUFFER
+    // bind point
+    setRectangle(
+        webGLContext, index * stripeWidth, 0, stripeWidth, canvas.height);
+
+    // Set a random color.
+    webGLContext.uniform4f(colorUniformLocation, COLORS[index].r, COLORS[index].g, COLORS[index].b, 1);
+    webGLContext.uniform1f(timeUniformLocation, Math.random());
+
+    // Draw the rectangle.
+    var primitiveType = webGLContext.TRIANGLES;
+    const offset = 0;
+    const count = 6;
+    webGLContext.drawArrays(primitiveType, offset, count);
+  }
 });
